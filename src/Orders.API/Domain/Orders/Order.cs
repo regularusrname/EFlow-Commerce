c600569bf301419b1020304ebac501c77d4a06e9
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-05-29 21:48:47 +0500

    refactor domain model for future database migration

--- src/Orders.API/Domain/Orders/Order.cs
+++ src/Orders.API/Domain/Orders/Order.cs
@@ -4,12 +4,16 @@ namespace Orders.API.Domain.Orders;
 
 public class Order
 {
+    private readonly List<OrderItem> _items = [];
+
+    private Order() {}
+
     [SetsRequiredMembers]
     public Order(Guid customerId, IEnumerable<OrderItem> items)
     {
         if (customerId == Guid.Empty)
             throw new ArgumentException("CustomerId of Order must exist");
-        if (items is null || items.Count() == 0)
+        if (items is null || !items.Any())
             throw new ArgumentException("Items of Order must exist");
 
         Id = Guid.CreateVersion7();
@@ -17,14 +21,14 @@ public class Order
         CreatedAtUtc = DateTime.UtcNow;
 
         CustomerId = customerId;
-        Items = [.. items];
+        _items = [.. items];
     }
     
     public required Guid Id { get; init; }
     public required Guid CustomerId { get; init; }
     public required OrderStatus Status { get; set; }
     public required DateTime CreatedAtUtc { get; init; }
-    public required List<OrderItem> Items { get; init; }
+    public IReadOnlyCollection<OrderItem> Items => _items;
     public decimal TotalPrice => CalculateTotalPrice();
 
     private decimal CalculateTotalPrice()
--- src/Orders.API/Domain/Orders/OrderItem.cs
+++ src/Orders.API/Domain/Orders/OrderItem.cs
@@ -4,14 +4,16 @@ namespace Orders.API.Domain.Orders;
 
 public class OrderItem
 {
+    private OrderItem() {}
+
     [SetsRequiredMembers]
-    public OrderItem(Guid productId, uint quantity, decimal unitPrice)
+    public OrderItem(Guid productId, int quantity, decimal unitPrice)
     {
-        if (quantity == 0)
+        if (quantity <= 0)
             throw new ArgumentException("Quantity of OrderItem should be greater then 0");
         if (unitPrice <= 0)
             throw new ArgumentException("UnitPrice of OrderItem should be greater then 0");
-        if (ProductId == Guid.Empty)
+        if (productId == Guid.Empty)
             throw new ArgumentException("ProductId of OrderItem must have an Id");
         
         Id = Guid.CreateVersion7();
@@ -22,7 +24,7 @@ public class OrderItem
 
     public required Guid Id { get; init; }
     public required Guid ProductId { get; init; } 
-    public required uint Quantity { get; set; } 
+    public required int Quantity { get; set; } 
     public required decimal UnitPrice { get; set; }
     public decimal TotalPrice => Quantity * UnitPrice;
 }
