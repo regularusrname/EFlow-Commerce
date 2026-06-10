ab8301770eb77738feced9b90eb9c94b88d1d16b
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-06-10 20:46:16 +0500

    order api unit tests: add some validation tests

--- src/tests/Orders.UnitTests/OrderValidationTests.cs
+++ src/tests/Orders.UnitTests/OrderValidationTests.cs
@@ -1,4 +1,7 @@
+using FluentValidation;
+using Orders.API.Common;
 using Orders.API.Features.CreateOrder;
+using Orders.API.Features.GetOrder;
 
 namespace Orders.UnitTests;
 
@@ -11,7 +14,7 @@ public class OrderValidationTests
     ];
 
     [Fact]
-    public async Task SuccessValidationResultWhenValidData()
+    public async Task CreateOrderValidation_Success_WithValidData()
     {
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
         var command = new CreateOrderCommand(Guid.CreateVersion7().ToString(), _validItems);
@@ -23,7 +26,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenCustomerIdIsEmptyGuid()
+    public async Task CreateOrderValidation_Failure_CustomerIdIsEmptyGuid()
     {
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
         var command = new CreateOrderCommand(Guid.Empty.ToString(), _validItems);
@@ -36,7 +39,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenCustomerIdIsWhiteSpace()
+    public async Task CreateOrderValidation_Failure_CustomerIdIsWhiteSpace()
     {
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
         var command = new CreateOrderCommand(" ", _validItems);
@@ -49,7 +52,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenCustomerIdIsEmptyString()
+    public async Task CreateOrderValidation_Failure_CustomerIdIsEmptyString()
     {
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
         var command = new CreateOrderCommand("", _validItems);
@@ -62,7 +65,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenCustomerIdIsInvalidGuid()
+    public async Task CreateOrderValidation_Failure_CustomerIdIsInvalidGuid()
     {
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
         var command = new CreateOrderCommand($"{Guid.CreateVersion7()}ab1", _validItems);
@@ -75,7 +78,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenItemsListIsEmpty()
+    public async Task CreateOrderValidation_Failure_ItemsListIsEmpty()
     {
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
         var command = new CreateOrderCommand(Guid.CreateVersion7().ToString(), []);
@@ -88,7 +91,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenOrderItemProductIdIsEmptyGuid()
+    public async Task CreateOrderValidation_Failure_ProductIdIsEmptyGuid()
     {
         List<CreateOrderItem> invalidItems = [.. _validItems, new(Guid.Empty.ToString(), 1, 1m)];
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
@@ -102,7 +105,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenOrderItemProductIdIsWhitespace()
+    public async Task CreateOrderValidation_Failure_ProductIdIsWhitespace()
     {
         List<CreateOrderItem> invalidItems = [.. _validItems, new(" ", 1, 1m)];
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
@@ -116,7 +119,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenOrderItemProductIdIsEmptyString()
+    public async Task CreateOrderValidation_Failure_ProductIdIsEmptyString()
     {
         List<CreateOrderItem> invalidItems = [.. _validItems, new("", 1, 1m)];
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
@@ -131,7 +134,7 @@ public class OrderValidationTests
 
 
     [Fact]
-    public async Task FailureResultWhenOrderItemProductIdIsInvalid()
+    public async Task CreateOrderValidation_Failure_ProductIdIsInvalid()
     {
         List<CreateOrderItem> invalidItems = [.. _validItems, new($"{Guid.CreateVersion7()}ab", 1, 1m)];
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
@@ -145,7 +148,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenQuantityInCreateOrderItemNotGreaterThanZero()
+    public async Task CreateOrderValidation_Failure_QuantityNotGreaterThanZero()
     {
         List<CreateOrderItem> invalidItems = [.. _validItems, new(Guid.CreateVersion7().ToString(), -1, 1m)];
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
@@ -159,7 +162,7 @@ public class OrderValidationTests
     }
 
     [Fact]
-    public async Task FailureResultWhenUnitPriceInCreateOrderItemNotGreaterThanZero()
+    public async Task CreateOrderValidation_Failure_UnitPriceNotGreaterThanZero()
     {
         List<CreateOrderItem> invalidItems = [.. _validItems, new(Guid.CreateVersion7().ToString(), 1, -1m)];
         var validator = new CreateOrderCommandValidator(new CreateOrderItemValidator());
@@ -170,5 +173,83 @@ public class OrderValidationTests
         Assert.False(response.IsValid);
         Assert.NotEmpty(response.Errors);
         Assert.Equal("UnitPrice should be greater than 0", response.Errors.First().ErrorMessage);
+    }
+
+    [Fact]
+    public async Task GetOrderValidation_Success_ValidData()
+    {
+        var orderId = Guid.CreateVersion7().ToString();
+        var validator = new GetOrderQueryValidator();
+        var query = new GetOrderQuery(orderId);
+
+        var response = await validator.ValidateAsync(query);
+
+        Assert.True(response.IsValid);
+        Assert.Empty(response.Errors);
+    }
+
+    [Fact]
+    public async Task GetOrderValidation_Failure_OrderIdIsEmptyGuid()
+    {
+        var orderId = Guid.Empty.ToString();
+        var validator = new GetOrderQueryValidator();
+        var query = new GetOrderQuery(orderId);
+
+        var response = await validator.ValidateAsync(query);
+
+        Assert.False(response.IsValid);
+        Assert.NotEmpty(response.Errors);
+    }
+
+    [Fact]
+    public async Task GetOrderValidation_Failure_OrderIdIsEmptyString()
+    {
+        var orderId = "";
+        var validator = new GetOrderQueryValidator();
+        var query = new GetOrderQuery(orderId);
+
+        var response = await validator.ValidateAsync(query);
+
+        Assert.False(response.IsValid);
+        Assert.NotEmpty(response.Errors);
+    }
+
+    [Fact]
+    public async Task GetOrderValidation_Failure_OrderIdIsWhiteSpace()
+    {
+        var orderId = " ";
+        var query = new GetOrderQuery(orderId);
+        var validator = new GetOrderQueryValidator();
+
+        var response = await validator.ValidateAsync(query);
+
+        Assert.False(response.IsValid);
+        Assert.NotEmpty(response.Errors);
+    }
+
+    [Fact]
+    public async Task GetOrderValidation_Failure_OrderIdIsNumber()
+    {
+        var orderId = 111;
+        var query = new GetOrderQuery(orderId.ToString());
+        var validator = new GetOrderQueryValidator();
+
+        var response = await validator.ValidateAsync(query);
+
+        Assert.False(response.IsValid);
+        Assert.NotEmpty(response.Errors);
+    }
+
+    [Fact]
+    public async Task GetOrderValidation_Failure_OrderIdIsObject()
+    {
+        var orderId = new { Name = "Object" };
+        var query = new GetOrderQuery(orderId.ToString()!);
+        var validator = new GetOrderQueryValidator();
+
+        var response = await validator.ValidateAsync(query);
+
+        Assert.False(response.IsValid);
+        Assert.NotEmpty(response.Errors);
     }
 }
