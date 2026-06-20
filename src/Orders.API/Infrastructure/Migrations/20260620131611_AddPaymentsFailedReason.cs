c30607e6a4e9957e5bd47a6ddab64e46102caef5
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-06-20 19:19:59 +0500

    order api: add new migration snapshot

---
+++ src/Orders.API/Infrastructure/Migrations/20260620131611_AddPaymentsFailedReason.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orders.API.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentsFailedReason : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentFailedReason",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentFailedReason",
+                table: "Orders");
+        }
+    }
+}
